a97e1daa7de40dc748bf4b907534cbc79d0f3976
Michael Fuget <michaelfuget@gmail.com>
2020-12-05 03:36:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,14 @@
-class Triangle
+cclass Triangle
   attr_accessor :side_a, :side_b, :side_c 
   def initialize(a, b, c)
     @side_a = a
+    @side_b = b
+    @side_c = c
+
   end
+  
+  def kind 
+    if(a==b && b==c)
+      return 
     
 end
